afcfef37a373b141ca0c9cc32a8570de1d17f693
Vladislav <Vladislav@DESKTOP-263V2B5>
2022-05-08 13:01:03 +0200

    Initial Commit

---
+++ IT Projekt HWS/desktop/src/com/itp/pacman/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.itp.pacman;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.itp.pacman.PacMan;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("PacMan");
+		new Lwjgl3Application(new PacMan(), config);
+	}
+}
