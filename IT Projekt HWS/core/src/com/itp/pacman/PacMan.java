614538ba85ad2ccc05280a4e607a1ec53298e915
ChupinVv <ChupinVv@D102-44.oes.ml-bw.de>
2022-05-11 11:07:25 +0200

    Added simple map generation

--- IT Projekt HWS/core/src/com/itp/pacman/PacMan.java
+++ IT Projekt HWS/core/src/com/itp/pacman/PacMan.java
@@ -6,26 +6,45 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class PacMan extends ApplicationAdapter {
+	
+	Texture[] tilemap = new Texture[1];
 	SpriteBatch batch;
-	Texture img;
+	Texture img0, img1;
+	
+	int[][] map = new int[][] {
+		{1, 0, 1, 1, 1, 0, 0},
+		{1, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 1},
+		{1, 0, 1, 1, 1, 0, 0}
+	};
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img0 = new Texture("empty.jpg");
+		img1 = new Texture("badlogic.jpg");
+		tilemap = new Texture[] {img0,img1};
 	}
 
 	@Override
 	public void render () {
+		
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		for(int i = 0; i < map.length; i++) {
+			for(int k = 0; k < map[i].length; k++) {
+				batch.draw(tilemap[map[i][k]], i*img0.getWidth(), k*img0.getHeight()); //img posx, posy
+			}
+		}
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		img1.dispose();
 	}
 }
