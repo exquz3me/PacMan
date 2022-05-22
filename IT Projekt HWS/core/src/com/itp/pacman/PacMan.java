d5b2a8658221a03a3c2558c49a6213a29c7251cc
Vladislav <Vladislav@DESKTOP-263V2B5>
2022-05-22 23:43:27 +0200

    Basic Screen sytsem (early commit)

--- IT Projekt HWS/core/src/com/itp/pacman/PacMan.java
+++ IT Projekt HWS/core/src/com/itp/pacman/PacMan.java
@@ -1,6 +1,7 @@
 package com.itp.pacman;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
@@ -15,224 +16,73 @@ import com.badlogic.gdx.utils.viewport.FillViewport;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
+import com.itp.pacman.screens.GameScreen;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.InputProcessor;
 
-public class PacMan extends ApplicationAdapter implements InputProcessor {
-	int[][] map = new int[][] {
-		{1, 0, 1, 1, 1, 0, 0},
-		{1, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 1},
-		{1, 0, 1, 1, 1, 0, 0}
-	};
-	//extends Game
+public class PacMan extends Game {
+	//this code applies to everything (shared properties)
 	
-	public SpriteBatch batch;
-	Sprite img;
-	OrthographicCamera camera;
-	Viewport viewport;
+	//passed on vars
+	private SpriteBatch batch;
+	private OrthographicCamera camera;
+	private Viewport viewport;
+	private BitmapFont font;
 	
-	
-	
-	final float WORLD_WIDTH = 100;		//virtuall cooridniate system
-	final float WORLD_HEIGHT = 100;		//https://sodocumentation.net/libgdx/topic/4219/supporting-multiple-resolutions
-	
-	BitmapFont font;
-	ShapeRenderer shapeRenderer;
-	Texture[] tilemap;
-	float circleX = 200;
-	float circleY = 100;
-	
-	Texture img0, img1;
-	
-    float r = MathUtils.random();
-    float g = MathUtils.random();
-    float b = MathUtils.random();
+	public final float WORLD_WIDTH = 100;		//virtuall cooridniate system
+	public final float WORLD_HEIGHT = 100;		//https://sodocumentation.net/libgdx/topic/4219/supporting-multiple-resolutions
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-	    img = new Sprite(new Texture("res.png"));
-	    img.setPosition(0,0);
-	    img.setSize(100,100);
+		setBatch(new SpriteBatch());    
+	    setCamera(new OrthographicCamera());
+	    setViewport(new FitViewport(WORLD_WIDTH, WORLD_HEIGHT,getCamera())); //default viewport
+	    setFont(new BitmapFont());
+	    setScreen(new GameScreen(this)); //passes a references of this class to the gamescreen
+	    //change gamescreen to main menu
 	    
-	    //float aspectRatio = (float)Gdx.graphics.getHeight()/(float)Gdx.graphics.getWidth();
-	    
-	    camera = new OrthographicCamera();
-	    viewport = new FillViewport(WORLD_WIDTH, WORLD_HEIGHT,camera); //FitViewport seems reasonably good for our use
-	    viewport.apply();
-	    
-		img0 = new Texture("empty.jpg");
-		img1 = new Texture("badlogic.jpg");
-		tilemap = new Texture[] {img0,img1};
-		font = new BitmapFont();
-		shapeRenderer = new ShapeRenderer();
-		//this.setScreen(new GameScreen(this));
-		
-		
-		/*Gdx.input.setInputProcessor(new InputAdapter(){
-			@Override
-			public boolean keyTyped (char key) {
-                r = MathUtils.random();
-                g = MathUtils.random();
-                b = MathUtils.random();
-				return true;
-			}
-			
-			@Override
-			public boolean touchDown (int x, int y, int pointer, int button) {
-                r = MathUtils.random();
-                g = MathUtils.random();
-                b = MathUtils.random();
-				return true;
-			}
-		});*/
-		
-	    Gdx.input.setInputProcessor(this);
-	}
-
-	@Override
-	public void render () {
-		
-	      Gdx.gl.glClearColor(1, 0, 0, 1);
-	      Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-	      camera.update();
-	      batch.setProjectionMatrix(viewport.getCamera().combined);
-	      batch.begin();
-	      img.draw(batch);
-	      
-	      //have two viewportsm one for the game, one for mouse input
-	      
-			shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);						//layer 2	
-			shapeRenderer.setProjectionMatrix(viewport.getCamera().combined);
-			shapeRenderer.setColor(r, g, b, 1);
-			shapeRenderer.circle(circleX, circleY, 25);
-			
-	      batch.end();
-	      
-	     
-			shapeRenderer.end();
-	    
-		/*
-		ScreenUtils.clear(1, 0, 0, 1);
-		//polling type input
-
-		
-		if(Gdx.input.isKeyPressed(Input.Keys.W)) {
-			circleY++;
-		}
-		if(Gdx.input.isKeyPressed(Input.Keys.S)) {
-			circleY--;
-		}
-		if(Gdx.input.isKeyPressed(Input.Keys.D)) {
-			circleX++;
-		}
-		if(Gdx.input.isKeyPressed(Input.Keys.A)) {
-			circleX--;
-		}
-		Gdx.gl.glClearColor(.25f, .25f, .25f, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		
-		
-		//imput event handler for single action keys that would be cumbersome to poll
-		
-		//render layers depend on code (last thing on top)
-			
-		batch.begin();	
-		for(int i = 0; i < map.length; i++) {
-			for(int k = 0; k < map[i].length; k++) {
-				batch.draw(tilemap[map[i][k]], i*img0.getWidth(), k*img0.getHeight()); //layer 3
-			}
-		}	
-		batch.end();
-		
-		batch.begin();	
-		font.draw(batch, "PAC MAN", circleX, circleY);								//layer 1
-		batch.end();
-		*/
+	    //tilemap object
+	   
+        
+        //TODO: window should be the fit viewport, you should only be able to resize the screen in the same apscet ratio
 	}
 	
 	@Override
-	public void dispose () {	
-		shapeRenderer.dispose();
-		batch.dispose();
-		img1.dispose();
-		font.dispose();
-	    img.getTexture().dispose();
-	}
-	
-	@Override
-	public void resize(int width, int height) {
-		viewport.update(width, height);
-	    camera.position.set(camera.viewportWidth/2,camera.viewportHeight/2,0);
-	    Gdx.app.log("resized", null);
+	public void dispose () {	//TODO: findout how dispose works, and what has to be disposed
+		getBatch().dispose();
+		getFont().dispose();
 	}
 
-	@Override
-	public boolean keyDown(int keycode) {
-		// TODO Auto-generated method stub
-		return false;
+	public SpriteBatch getBatch() {
+		return batch;
+	}
+	public void setBatch(SpriteBatch batch) {
+		this.batch = batch;
 	}
 
-	@Override
-	public boolean keyUp(int keycode) {
-		// TODO Auto-generated method stub
-		return false;
+	public Viewport getViewport() {
+		return viewport;
+	}
+	public void setViewport(Viewport viewport) {
+		this.viewport = viewport;
 	}
 
-	@Override
-	public boolean keyTyped(char character) {
-		// TODO Auto-generated method stub
-		return false;
+	public OrthographicCamera getCamera() {
+		return camera;
+	}
+	public void setCamera(OrthographicCamera camera) {
+		this.camera = camera;
 	}
 
-	@Override
-	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-	      Gdx.app.log("Mouse Event","Click at " + screenX + "," + screenY);
-	      Vector3 worldCoordinates = camera.unproject(new Vector3(screenX,screenY,0));
-	      Gdx.app.log("Mouse Event","Projected at " + worldCoordinates.x + "," + worldCoordinates.y);
-	      
-          r = MathUtils.random();
-          g = MathUtils.random();
-          b = MathUtils.random();
-          
-          circleX = worldCoordinates.x;
-          circleY = worldCoordinates.y;
-          
-	      Gdx.app.log("circleX", ""+ circleX);
-	      Gdx.app.log("circleY", ""+ circleY);
-          
-	      return false;
+	public BitmapFont getFont() {
+		return font;
 	}
 
-	@Override
-	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean touchDragged(int screenX, int screenY, int pointer) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean mouseMoved(int screenX, int screenY) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean scrolled(float amountX, float amountY) {
-		// TODO Auto-generated method stub
-		return false;
+	public void setFont(BitmapFont font) {
+		this.font = font;
 	}
 }
--- IT Projekt HWS/core/src/com/itp/pacman/screens/GameScreen.java
+++ IT Projekt HWS/core/src/com/itp/pacman/screens/GameScreen.java
@@ -1,19 +1,39 @@
 package com.itp.pacman.screens;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.itp.pacman.PacMan;
 
-public class GameScreen implements Screen{
-
-	Texture img;
-	float xpos;
-	float ypos;
-	float speed = 40;
+public class GameScreen extends ScreenAdapter implements InputProcessor {
+	//other screens will exitend this class, code in here will apply to every other screen
+	//-> add button and sub menu support
 	
-	PacMan game;
+	PacMan game; //a reference to the shared variables
+	private SpriteBatch batch;
+	private OrthographicCamera camera;
+	private Viewport viewport;
+	private BitmapFont font;
+
+	//Debug
+	private ShapeRenderer shapeRenderer;
+	Sprite img;
+	float speed = 10f;
 	
 	public GameScreen(PacMan game) {
 		this.game = game;
@@ -21,41 +41,50 @@ public class GameScreen implements Screen{
 	
 	@Override
 	public void show() {
-		img = new Texture("badlogic.jpg");
+		batch = game.getBatch();
+		camera = game.getCamera();
+		viewport = game.getViewport();
+		font = game.getFont();
+        Gdx.input.setInputProcessor(this);
+		//add camera movement for debug
+	    //setScreen(new XScreen(game));
+		//add button support
+	    
+		//Debug
+	    img = new Sprite(new Texture("res.png"));
+	    img.setPosition(0,0);
+	    img.setSize(100,100);	    
 	}
+	
+	//float aspectRatio = (float)Gdx.graphics.getHeight()/(float)Gdx.graphics.getWidth();
+    //it should be the fit viewport, you should only be able to resize the screen in the same apscet ratio
+	
+	//viewport = new FitViewport(game.WORLD_WIDTH, game.WORLD_HEIGHT,camera);  //this screens individual viewport
+    //game.setViewport(viewport);
+    //viewport.apply();
 
 	@Override
 	public void render(float delta) {
-		ScreenUtils.clear(1, 0, 0, 1);
+		Gdx.gl.glClearColor(.25f, .25f, .25f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);  
 		
-		//delta time is the time between frames
-		if(Gdx.input.isKeyPressed(Keys.UP)) {
-			ypos += speed * Gdx.graphics.getDeltaTime();
-		}
+		batch.setProjectionMatrix(viewport.getCamera().combined);
+	    camera.update();
+	    batch.begin();
+        img.draw(batch);
+	    font.draw(batch, "Title Screen!", 10, 10);
+        font.draw(batch, "Press Enter to play.", 12, 12);
+	    batch.end();
+	    			
+		//render layers depend on code (last thing on top)
 		
-		if(Gdx.input.isKeyPressed(Keys.DOWN)) {
-			ypos -= speed * Gdx.graphics.getDeltaTime();
-		}
-	
-		if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
-			xpos += speed * Gdx.graphics.getDeltaTime();
-		}
-		
-		if(Gdx.input.isKeyPressed(Keys.LEFT)) {
-			xpos -= speed * Gdx.graphics.getDeltaTime();
-		}	
-
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(0);
-		game.batch.begin();
-		game.batch.draw(img, xpos, ypos); //image, x cord, y cord
-		game.batch.end();
+		//Gdx.graphics.getDeltaTime()
 	}
 
 	@Override
 	public void resize(int width, int height) {
-		// TODO Auto-generated method stub
-		
+		viewport.update(width, height);
+	    camera.position.set(camera.viewportWidth/2, camera.viewportHeight/2, 0);
 	}
 
 	@Override
@@ -77,9 +106,74 @@ public class GameScreen implements Screen{
 	}
 
 	@Override
-	public void dispose() {
-		// TODO Auto-generated method stub
-		
+	public void dispose () {	
+		batch.dispose();
+		font.dispose();
+	    img.getTexture().dispose();
+	}
+    
+	@Override
+	public boolean keyDown(int keycode) {
+		if (keycode == Input.Keys.ENTER) {
+            //game.setScreen(new MenuScreen(game));
+        	Gdx.app.log("Enter", null);
+        }
+        
+        if (keycode == Input.Keys.NUM_8) //up
+        	camera.translate(0, speed * Gdx.graphics.getDeltaTime());
+        if (keycode == Input.Keys.NUM_2) //down
+        	camera.translate(0,-speed * Gdx.graphics.getDeltaTime());
+        if (keycode == Input.Keys.NUM_4) //left
+        	camera.translate(speed * Gdx.graphics.getDeltaTime(), 0);
+        if (keycode == Input.Keys.NUM_6) //right
+        	camera.translate(-speed * Gdx.graphics.getDeltaTime(),0);
+        if (keycode == Input.Keys.NUM_5) //center
+        	camera.position.set(camera.viewportWidth/2, camera.viewportHeight/2, 0);
+        
+		return false;
 	}
 
+	@Override
+	public boolean keyUp(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+    @Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+	      Gdx.app.log("Mouse Event","Click at " + screenX + "," + screenY);
+	      Vector3 worldCoordinates = camera.unproject(new Vector3(screenX,screenY,0));
+	      Gdx.app.log("Mouse Event","Projected at " + worldCoordinates.x + "," + worldCoordinates.y);              
+	      return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(float amountX, float amountY) {
+		// TODO Auto-generated method stub
+		return false;
+	}	
 }
